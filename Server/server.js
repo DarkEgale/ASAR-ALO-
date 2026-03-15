c6d396070a2d3a51be130de3de4ac521228759dc
DarkEgale <darkegale3536@gmail.com>
2026-03-15 10:43:49 -0400

    host update

--- Server/server.js
+++ Server/server.js
@@ -60,7 +60,7 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 5001;
-server.listen(PORT, () => {
+server.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
